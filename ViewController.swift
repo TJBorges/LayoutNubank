503f1e20463c83453f15d803f7cc0163356ab430
TJ <tj.borges3@gmail.com>
2021-04-01 21:00:37 -0300

    Finalizando layout

--- ViewController.swift
+++ ViewController.swift
@@ -19,9 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbSdDisponivel: UILabel!
     @IBOutlet weak var sdDisponivel: UILabel!
     
-    
     @IBOutlet weak var lbVlDisponivel: UILabel!
     @IBOutlet weak var vlDisponivel: UILabel!
+    
+    @IBOutlet weak var lbTotalRewards: UILabel!
+    @IBOutlet weak var totalRewards: UILabel!
+    @IBOutlet weak var lbInfo: UILabel!
+    @IBOutlet weak var lbTtotalMes: UILabel!
+    @IBOutlet weak var lbInfo2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +45,13 @@ class ViewController: UIViewController {
             
             lbVlDisponivel.isHidden = false
             vlDisponivel.isHidden = false
+            
+            lbTotalRewards.isHidden = false
+            totalRewards.isHidden = false
+            lbInfo.isHidden = false
+            lbTtotalMes.isHidden = false
+            lbInfo2.isHidden = false
+            
         } else{
             lbFaturaAtual.isHidden = true
             FtAtual.isHidden = true
@@ -51,6 +63,12 @@ class ViewController: UIViewController {
             
             lbVlDisponivel.isHidden = true
             vlDisponivel.isHidden = true
+            
+            lbTotalRewards.isHidden = true
+            totalRewards.isHidden = true
+            lbInfo.isHidden = true
+            lbTtotalMes.isHidden = true
+            lbInfo2.isHidden = true
         }
     }
     
